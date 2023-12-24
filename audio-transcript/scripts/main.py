af29f10852b68e69779dccaf83db83724e0f038a
Eugene <zhokaogurca@gmail.com>
2023-12-24 23:28:33 +0300

    Eugene, added env vars, hope that everything's ok

--- audio-transcript/scripts/main.py
+++ audio-transcript/scripts/main.py
@@ -4,68 +4,87 @@ import os
 import sys
 import pika
 from pika.exchange_type import ExchangeType
-from moviepy import VideoFileClip
+from moviepy.editor import VideoFileClip
 from faster_whisper import WhisperModel
 
-credentials = pika.credentials.PlainCredentials(username='videodetector',
-                                                password='123456')
+# RambbitMQ Settings
+username = os.getenv("RABBITMQ_DEFAULT_USER")
+password = os.getenv("RABBITMQ_DEFAULT_PASS")
+rabbit_host = os.getenv("RABBITMQ_DEFAULT_HOST")
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq",
+queue_in_name = os.getenv("QUEUE_IN_NAME")
+queue_out_name = os.getenv("QUEUE_OUT_NAME")
+exchange_name = os.getenv("EXCHANGE_NAME")
+routing_key_in = os.getenv("ROUTUNG_KEY_IN")
+routing_key_out = os.getenv("ROUTING_KEY_OUT")
+
+# Faster-Whisper Settings
+model_size = str(os.getenv("MODEL_SIZE"))
+device = str(os.getenv("DEVICE"))
+computer_type = str(os.getenv("COMPUTE_TYPE"))
+beam_size = int(os.getenv("BEAM_SIZE"))
+
+credentials = pika.credentials.PlainCredentials(username=username,
+                                                password=password)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host,
                                                                credentials=credentials))
 
-queue_in_name = "audio.transcription.queue.in"
-queue_out_name = "audio.transcription.queue.out"
-exchange_name = "audio.transcription.exchange"
-routing_key_in = "audio.transcript.in"
-routing_key_out = "audio.transcript.out"
 
+class StorageService:
+    def __init__(self, path):
+        self.__path = path
+        self.answer = []
+    def get_video_path(self, uuid):
+        self.__video_path = self.__path + "/storage/" + uuid + "/"
+    def get_source(self):
+        self.__source = self.__video_path + "source"
+    def split_source(self):
+        video_clip = VideoFileClip(rf"{self.__source}")
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(self.__video_path + "audio.mp3")
+        video_clip.without_audio().write_videofile(self.__video_path +"video.mp4")
+    def generate_subs(self, uuid):
+        model = WhisperModel(model_size, device="cuda", compute_type="float32")
+        segments, info = model.transcribe(self.__video_path + "audio.mp3", beam_size=5, word_timestamps=True)
+        for segment in segments:
+            for word in segment.words:
+                segment_subs = {
+                "uuid": bytes(uuid).decode("UTF-8"),
+                "words": [{
+                    "word": word.word,
+                    "start": word.start,
+                    "end": word.end
+                }]}
+                self.answer.append(segment_subs)
 
 def callback(ch, method, properties, body):
     print(f" [x] Received   {str(body)}")
     print(f" [x] Method     {str(method)}")
     print(f" [x] Properties {str(properties)}")
 
-    # TODO place code here.
     current_directory = os.path.dirname(os.path.realpath(__file__))
     parent_directory = os.path.dirname(current_directory)
     grandparent_directory = os.path.dirname(parent_directory)
     if grandparent_directory not in sys.path:
         sys.path.append(grandparent_directory)
+    uuid = str(body)
 
     print(f" [x] Received   {str(body)}")
     print(f" [x] Method     {str(method)}")
     print(f" [x] Properties {str(properties)}")
 
     # TODO place code here.
-    video = body.name
-    path_to_vid_dir = current_directory+"/storage/"+str(body.id)
-    path_to_original_vid = current_directory+"/storage/"+str(body.id)+"/"+video
-    print(path_to_original_vid)
-    print(video)
-    video_clip = VideoFileClip(rf"{path_to_original_vid}")
-    audio_clip = video_clip.audio
-    audio_clip.write_audiofile(path_to_vid_dir+ "/"+ video+'_audio.mp3')
-    video_clip.without_audio().write_videofile(path_to_vid_dir+ "/"+video+'_mute.mp4')
-    model_size = "medium"
-    model = WhisperModel(model_size, device="cuda", compute_type="float32")
-    segments, info = model.transcribe(path_to_vid_dir+ "/"+ video+'_audio.mp3', beam_size=5, word_timestamps=True)
-    answer = []
-    
-    for segment in segments:
-        for word in segment.words:
-            segment_subs = {
-            "uuid": bytes(body.id).decode("UTF-8"),
-            "words": [{
-                "word": word.word,
-                "start": word.start,
-                "end": word.end
-            }]}
-            answer.append(segment_subs)
-    
+    storage = StorageService(path=grandparent_directory)
+    storage.get_video_path(uuid=uuid)
+    storage.get_source()
+    storage.split_source()
+    storage.generate_subs(uuid=uuid)
+
     ch.basic_publish(exchange=exchange_name,
                      routing_key=routing_key_out,
-                     body=json.dumps(answer).encode("UTF-8"))
-
+                     body=json.dumps(storage.answer).encode("UTF-8"))
+    print(storage.answer)
 
 if __name__ == '__main__':
     channel = connection.channel()
